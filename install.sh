21273643899b25fe6a57ffe58e6695d97f94e9c2
Varyshen <varyshenstudios@proton.me>
2025-11-05 01:17:01 -0600

    Tweak install script layout

--- install.sh
+++ install.sh
@@ -3,7 +3,27 @@
 ###Farshief's Arch Install Script###
 
 ###Variables###
+INSTALL_DRIVE="/dev/vda"
+
+PARTITION_UEFI_SIZE=1G
+PARTITION_SWAP_SIZE=8G
+#PARTITION_ROOT_SIZE=
+
+
 
 ###Functions###
+function partition_drive() {
+	echo "Partition Drive: "
+}
+
+function create_filesystems() {
+	echo "Creating UEFI filesystem on ${INSTALL_DRIVE}1"
+	mkfs.fat -F 32 "${INSTALL_DRIVE}"1
+
+	echo "Creating Swap"
+	makeswap "${INSTALL_DRIVE}"2
+}
 
 ###Script###
+echo "Ensuring Connectivity to the Internet..."
+ping -c 3 archlinux.org
